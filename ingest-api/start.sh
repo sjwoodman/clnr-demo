318802fc82acd8fe7138fa3aaefa0e1b2b7ee65d
Simon <s.j.woodman@gmail.com>
2018-05-16 09:52:55 +0100

    Removed old project and updated docs

--- ingest-api/start.sh
+++ ingest-api/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-[ -z "$PRODUCER_URL" ] && echo "Need to set PRODUCER_URL" && exit 1;
+[ -z "$INGEST_URL" ] && echo "Need to set PRODUCER_URL" && exit 1;
 
-echo "Sending data to ${PRODUCER_URL}"
+echo "Sending data to ${INGEST_URL}"
 cat clnr.txt | while read line
 do
     if [[ $line != \#* ]] ; then
 
         echo $line;
-        curl -d "$line" -H "Content-Type: text/plain" -s -o /dev/null -w "%{http_code}\n" -X POST http://${PRODUCER_URL}/clnr/reading/csv
+        curl -d "$line" -H "Content-Type: text/plain" -s -o /dev/null -w "%{http_code}\n" -X POST http://${INGEST_URL}/clnr/reading/csv
         sleep 0.2
 
     fi
--- persistent-producer/src/main/java/com/redhat/demo/clnr/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.demo.clnr.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-}
